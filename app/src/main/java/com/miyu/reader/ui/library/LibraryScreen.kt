1402c62b8a120bdee41eacd711757db5157df454
asanlamma-ux <asanlamma@gmail.com>
2026-05-02 18:51:44 +0800

    Fix library Checkbox import

--- app/src/main/java/com/miyu/reader/ui/library/LibraryScreen.kt
+++ app/src/main/java/com/miyu/reader/ui/library/LibraryScreen.kt
@@ -59,6 +59,7 @@ import androidx.compose.material3.AssistChip
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
